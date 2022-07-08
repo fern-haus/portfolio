f51d5de9b02d030f3c1b3db11280f272707e2c45
Alec <fernandes.alec@gmail.com>
2022-07-08 13:03:06 -0700

    style portfolio homepage

--- src/components/Portfolio/Home.js
+++ src/components/Portfolio/Home.js
@@ -1,26 +1,28 @@
+import "../../css/home.css";
 import React from "react";
 import { projects } from "./SelectProject";
 
 export default function Home({ setProject }) {
     return (
         <div id="home">
-            <h1>Home</h1>
             {Object.keys(projects)
                 .slice(1)
                 .map((projectType) => (
                     <React.Fragment key={projectType}>
                         <h2>{projectType}</h2>
-                        {Object.keys(projects[projectType]).map(
-                            (projectName) => (
-                                <button
-                                    key={`homepage button for ${projectName}`}
-                                    className="button-link"
-                                    onClick={() => setProject(projectName)}
-                                >
-                                    {projectName}
-                                </button>
-                            )
-                        )}
+                        <div className="project-buttons">
+                            {Object.keys(projects[projectType]).map(
+                                (projectName) => (
+                                    <button
+                                        key={`homepage button for ${projectName}`}
+                                        className="button-link"
+                                        onClick={() => setProject(projectName)}
+                                    >
+                                        {projectName}
+                                    </button>
+                                )
+                            )}
+                        </div>
                     </React.Fragment>
                 ))}
         </div>
